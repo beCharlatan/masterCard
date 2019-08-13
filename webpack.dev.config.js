aab7790fdfe720068ee5fe4bc594eff177e165cf
Artyom Petrov <apetroov@gmail.com>
2019-08-13 16:26:29 +0300

    init webpack project

---
+++ webpack.dev.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.config')
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'production',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    overlay: true,
+    port: 9000
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[name].map'
+    })
+  ]
+})
+
+module.exports = new Promise((resolve, reject) => {
+  resolve(devWebpackConfig)
+})
