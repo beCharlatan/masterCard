9eafb2bc5bed1da44e14677918c08b638d02109d
Artyom Petrov <apetroov@gmail.com>
2019-08-14 12:56:22 +0300

    style main content section

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,6 +6,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
   devtool: 'cheap-module-eval-source-map',
   devServer: {
+    contentBase: baseWebpackConfig.externals.paths.src,
     overlay: true,
     port: 9000
   },
